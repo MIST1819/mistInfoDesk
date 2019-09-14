3d99aa9abddd4c3d8ae5328560f554406f5687ef
Yash Aryan <yasharyan307@outlook.com>
2019-09-14 10:05:20 +0530

    Cross platform compatibility improved
    'cls' does not work on Linux systems. But 'clear' does work on windows.  Changed that

--- MistInfoDesk.py
+++ MistInfoDesk.py
@@ -4,7 +4,7 @@ import json
 import sys
 import pyfiglet
 
-os.system('cls')
+os.system('clear')
 
 ascii_banner = pyfiglet.figlet_format("We are MIST!!")
 for l in ascii_banner:
@@ -22,11 +22,11 @@ for l in text1:
 name = input("What is your name?: ")
 print("\n")
 time.sleep(1)
-os.system('cls')
+os.system('clear')
 name_banner = pyfiglet.figlet_format(name)
 print(name_banner)
 time.sleep(1)
-os.system('cls')
+os.system('clear')
 
 # email
 print(name_banner)
@@ -34,12 +34,12 @@ print("\n")
 email = input("Tell us your email: ")
 print("\n")
 time.sleep(1)
-os.system('cls')
+os.system('clear')
 print(name_banner)
 print("\n")
 print("Recorded email:  " + email)
 time.sleep(1)
-os.system('cls')
+os.system('clear')
 
 # phone number
 print(name_banner)
@@ -47,12 +47,12 @@ print("\n")
 phoneNo = input("Your phone number : ")
 print("\n")
 time.sleep(1)
-os.system('cls')
+os.system('clear')
 print(name_banner)
 print("\n")
 print("Recorded phone  +91" + phoneNo)
 time.sleep(1)
-os.system('cls')
+os.system('clear')
 
 # registration number
 print(name_banner)
@@ -60,12 +60,12 @@ print("\n")
 regNo = input("Your registration number : ")
 print("\n")
 time.sleep(1)
-os.system('cls')
+os.system('clear')
 print(name_banner)
 print("\n")
 print("Recorded registration number " + regNo)
 time.sleep(1)
-os.system('cls')
+os.system('clear')
 
 # branch
 print(name_banner)
@@ -73,12 +73,12 @@ print("\n")
 branch = input("Branch : ")
 print("\n")
 time.sleep(1)
-os.system('cls')
+os.system('clear')
 print(name_banner)
 print("\n")
 print("Recorded branch " + branch)
 time.sleep(1)
-os.system('cls')
+os.system('clear')
 
 print(name_banner)
 print("\n")
@@ -88,7 +88,7 @@ for l in wait_message:
     sys.stdout.flush()
     time.sleep(0.1)
 time.sleep(2)
-os.system('cls')
+os.system('clear')
 
 recorder_Info = "All your responses were recorded successfully.\n But they are not saved yet.\n Verify your entry by typing yes"
 for l in recorder_Info:
@@ -108,7 +108,7 @@ print("\n")
 print("\n")
 confirmMess = input("Is this correct? Typy 'Yes' or 'No': ")
 if confirmMess == "yes" or confirmMess == "Yes":
-    os.system('cls')
+    os.system('clear')
     print("Good. Your messages were recorded. It's been saved on our system. See you in the recruitments.")
     deskCode = {
         "name": name,
@@ -121,13 +121,13 @@ if confirmMess == "yes" or confirmMess == "Yes":
         json.dump(deskCode, json_file)
 elif confirmMess == "no":
     print("Alright. We are redirecting you to the form again. This time, don't make an error.")
-    os.system('cls')
+    os.system('clear')
     os.system('python MistInfoDesk.py')
 else:
     print("Errors are the root of all evil and this is what you just did. Now refill the form")
 
 time.sleep(5)
-os.system('cls')
+os.system('clear')
 reStart = input("Use mancomm code to quit, else type anything else:  ")
 if reStart == 3000:
     print("The commmand ends now")
